cd686070391d98acee773465a9cdb2d406facfcf
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-05 22:59:34 +0300

    [Feat] update

---
+++ Lecture7/Presentation/Lecture7.API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Week7.Domain.Entities;
+using Week7.Persitence.Contexts;
+
+namespace Lecture7.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Week7_DbContext _context;
+
+        public UsersController()
+        {
+            _context = new();
+        }
+
+        [HttpGet]
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
