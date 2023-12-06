1a1063bc66f3f65d17659c72223f3fcde66b33a8
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-06 21:48:12 +0300

    Week10-Singleton3

---
+++ Week10_1/Infrastructure/Week10_1.Infrastructure/Services/ConfigurationService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week10_1.Infrastructure.Services
+{
+   public class ConfigurationService
+    {
+        private static ConfigurationService instance;
+        public static ConfigurationService GetInstance()
+        {
+            if (instance is null)
+                instance = new ConfigurationService();
+
+            return instance;
+        }
+        public string GetValue(string key)
+        {
+            ConfigurationManager configurationManager = new();
+            string path = Directory.GetCurrentDirectory();
+            configurationManager.SetBasePath(path);
+            configurationManager.AddJsonFile("appsettings.json");
+
+            return configurationManager.GetSection(key).Value;
+        }
+        ConfigurationService()
+        {
+            Console.WriteLine("Instance created.");
+        }
+    }
+}
---
+++ Week10_1/Presentation/Week10_1.API/Controllers/CustomersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Week10_1.Infrastructure.Services;
+
+namespace Week10_1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public CustomersController()
+        {
+            
+        }
+
+        [HttpGet]
+        public void Get(string name)
+        {
+            ConfigurationService.GetInstance().GetValue(name);
+        }
+    }
+}
