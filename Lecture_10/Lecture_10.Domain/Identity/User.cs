5c5a411fac42e189ad44988ccd6be690c2920ae3
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-12-07 19:41:03 +0300

    [Feat] Updated Identity Class

---
+++ Lecture_10/Lecture_10.Domain/Identity/User.cs
@@ -0,0 +1,27 @@
+﻿using Lecture_10.Domain.Entities;
+using Lecture_10.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_10.Domain.Identity
+{
+    public class User : IdentityUser<Guid>, IEntityBase<Guid>, ICreatedByEntity, IModifiedByEntity
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+
+        public DateTimeOffset? BirthDate { get; set; }
+        public Gender Gender { get; set; }
+
+        public UserSetting UserSetting { get; set; }
+
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+        public DateTimeOffset? LastModifiedOn { get; set; }
+    }
+}
---
+++ Lecture_10/Lecture_10.Domain/Identity/UserSetting.cs
@@ -0,0 +1,17 @@
+﻿using Lecture_10.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_10.Domain.Identity
+{
+    public class UserSetting : EntityBase<Guid>
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Int16 TimeZone { get; set; }
+        public string LanguageCode { get; set; }
+    }
+}
