82b6a1755899ac3d8de18427bc2179b132de71a6
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-01 22:20:37 +0300

    [Feat] Added ProductsController

---
+++ Lecture_13/Presentation/Lecture_13.API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Lecture_13.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Lecture_13.Persistence.DbContext;
+
+namespace Lecture_13.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly Lecture_13_DbContext _context;
+
+        public ProductsController(Lecture_13_DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
