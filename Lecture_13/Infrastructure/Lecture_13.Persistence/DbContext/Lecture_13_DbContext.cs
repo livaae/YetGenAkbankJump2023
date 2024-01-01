3911c9d7bfe832c5017a9d22a7706c6c37b15597
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-01 22:20:19 +0300

    [Feat] Add DbContext and API

---
+++ Lecture_13/Infrastructure/Lecture_13.Persistence/DbContext/Lecture_13_DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture_13.Domain.Entities;
+
+namespace Lecture_13.Persistence.DbContext
+{
+    public class Lecture_13_DbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Lecture_13"); 
+        }
+
+    }
+}
---
+++ Lecture_13/Presentation/Lecture_13.API/Program.cs
@@ -0,0 +1,28 @@
+using Lecture_13.Persistence.DbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Lecture_13_DbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
