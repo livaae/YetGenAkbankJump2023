ed106895342764adeca63faec0818ba21275be74
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2024-01-04 22:39:29 +0300

    [Feat] Updated IRepositories

---
+++ Lecture_13/Core/Lecture_13.Application/Repositories/CustomerRepositories/ICustomerReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using Lecture_13.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_13.Application.Repositories.CustomerRepositories
+{
+    public interface ICustomerReadRepository<T> : IReadRepository<T> where T : EntityBase
+    {
+    }
+}
--- Lecture_13/Core/Lecture_13.Application/Repositories/ProductRepositories/IProductWriteRepository.cs
+++ Lecture_13/Core/Lecture_13.Application/Repositories/ProductRepositories/IProductWriteRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Lecture_13.Application.Repositories.ProductRepositories
 {
-    public interface IProductWriteRepository : IWriteRepository<Product>
+    public interface IProductWriteRepository<T> : IWriteRepository<Product>
     {
     }
 }
